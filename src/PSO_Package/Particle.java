21e4018ed567740cf30ce84d3e9720615dd0d96e
sai preetham reddy alladu <alladu.s@husky.neu.edu>
2019-08-04 23:34:42 -0400

    particle and optmizer

--- src/PSO_Package/Particle.java
+++ src/PSO_Package/Particle.java
@@ -5,10 +5,34 @@
  */
 package PSO_Package;
 
+import java.util.Random;
+
 /**
  *
  * @author Preetham Reddy
  */
 public class Particle {
+    double[] xSolution;
+    double xFitnessValue;
+    double[] pBest;
+    double pBestValue;
+    double[] pBestVelocity;
+    double[] pVelocity;
     
+    public Particle(){
+        
+    }
+    
+    
+    public void setRandomVelcities(int n) {
+        this.pVelocity = new double[n];
+        for (int i = 0; i < n; i++) {
+                this.pVelocity[i] = getRandomVelocity(n);
+        }
+    }
+
+    private double getRandomVelocity(int upper) {
+            int lower = 0;
+            return (new Random().nextDouble() * (upper - lower)) + lower;
+    }
 }
